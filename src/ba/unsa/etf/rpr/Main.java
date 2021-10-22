a362087e684d90256018f42c1c4402c7106c9522
dmuhic1 <dmuhic1@etf.unsa.ba>
2021-10-22 13:35:18 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Sat s=new Sat(16,30,45);
+    s.Ispisi();
+    s.Sljedeci();
+    s.Ispisi();
+    s.PomjeriZa(-40);
+    s.Ispisi();
+    s.Postavi(1,2,3);
+    s.Ispisi();
+    }
+}
